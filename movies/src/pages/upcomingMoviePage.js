869a77a1b8069db81e769151f7b4c3863c35a90c
JRDNVI <20096529.wit@gmail.com>
2023-11-03 20:55:53 +0000

    Changed upcoming movies to use caching.

--- movies/src/pages/upcomingMoviePage.js
+++ movies/src/pages/upcomingMoviePage.js
@@ -3,14 +3,20 @@ import PageTemplate from '../components/templateMovieListPage'
 import { getUpcomingMovies } from "../api/tmdb-api";
 import AddToPlaylistIcon from "../components/cardIcons/addToPlaylist";
 
-const UpcomingMoviePage = (props) => {
-  const [movies, setMovies] = useState([]);
+import { useQuery } from "react-query";
+import Spinner from '../components/spinner';
 
-  useEffect(() => {
-    getUpcomingMovies().then(movies => {
-      setMovies(movies);
-    });
-  }, []);
+const UpcomingMoviePage = (props) => {
+  const { data, error, isLoading, isError } = useQuery('upcoming', getUpcomingMovies)
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
 
   return (
     <PageTemplate
